e5b4170f3453955ceca30b69ac4f2205685ed077
Giorgio Giannotta <jorge_upa@msn.com>
2021-06-08 10:25:42 +0200

    Initial Commit

---
+++ Scratch&Win/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scratch&Win
+//
+//  Created by Jorge Giannotta on 08/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scratch&Win/Scratch_WinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scratch_WinApp.swift
+//  Scratch&Win
+//
+//  Created by Jorge Giannotta on 08/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Scratch_WinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
